a063dba388ee6fa03457db91de24d007a436bd4e
Herwiggg <michelot.alexy78@gmail.com>
2025-11-20 19:28:49 +0100

    ex07

---
+++ Starting/ex07/sos.py
@@ -0,0 +1,29 @@
+import sys
+
+def translate_morse(string: str) -> bool:
+    NESTED_MORSE = {
+    " ": "/ ", "A": ".- ", "B": "-... ", "C": "-.-. ", "D": "-.. ",
+    "E": ". ", "F": "..-. ", "G": "--. ", "H": ".... ", "I": ".. ",
+    "J": ".--- ", "K": "-.- ", "L": ".-.. ", "M": "-- ", "N": "-. ",
+    "O": "--- ", "P": ".--. ", "Q": "--.- ", "R": ".-. ", "S": "... ",
+    "T": "- ", "U": "..- ", "V": "...- ", "W": ".-- ", "X": "-..- ",
+    "Y": "-.-- ", "Z": "--.. ", "0": "----- ", "1": ".---- ", 
+    "2": "..--- ", "3": "...-- ", "4": "....- ", "5": "..... ",
+    "6": "-.... ", "7": "--... ", "8": "---.. ", "9": "----. ",
+    }
+    string = string.upper() 
+    result = ""
+    for c in string:
+        if c in NESTED_MORSE:
+            result += NESTED_MORSE[c]
+        else:
+            return False
+    print(result)
+    return True
+
+def main():
+    assert len(sys.argv) == 2, "the arguments are bad"
+    assert translate_morse(sys.argv[1]) == True, "the arguments are bad"
+
+if __name__ == "__main__":
+    main()
---
+++ Starting/ex08/Loading.py
@@ -0,0 +1,13 @@
+
+
+def ft_tqdm(lst: range)-> None:
+    
+
+
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
